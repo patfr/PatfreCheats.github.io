7ab28325839d742cb7a88130e16317d424fd01ad
patfre <56884994+patfr@users.noreply.github.com>
2021-09-25 13:57:13 +0200

    Create Item.js

---
+++ SynergismSaveEditor/classes/Item.js
@@ -0,0 +1,42 @@
+class Item {
+    constructor(Values, Current) {
+        this.Name = Values.Name;
+        this.Index = Values.Index;
+        this.Type = Values.Type;
+        this.Current = Current;
+
+        this.CV = document.getElementById(`${this.Index}EditCV`);
+        this.Input0 = document.getElementById(`${this.Index}Input0`);
+        this.Input1 = document.getElementById(`${this.Index}Input1`);
+        
+        this.Update();
+    }
+
+    Update() {
+        if (loaded) {
+            this.Current = jsonData[this.Index];
+        }
+
+        switch(this.Type) {
+            case "number":
+                let strs = this.Current.toString().split("e+");
+                if (strs.length == 2) {
+                    this.CV.innerHTML = `Current: ${strs[0]}e+${Number(strs[1])}`;
+                    this.Input0.value = strs[0];
+                    if (this.Input1 != undefined) {
+                        this.Input1.value = strs[1];
+                    }
+                } else if (strs.length == 1) {
+                    let length = this.Current.toString().length - 1;
+                    this.CV.innerHTML = `Current: ${this.Current / Math.pow(10, length)}e+${length}`;
+                    this.Input0.value = this.Current / Math.pow(10, length);
+                    if (this.Input1 != undefined) {
+                        this.Input1.value = length;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
