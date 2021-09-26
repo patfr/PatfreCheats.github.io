f9bff67d3699f4e3d5b3a992ee5883488fd5b63a
patfre <56884994+patfr@users.noreply.github.com>
2021-09-26 16:05:35 +0200

    Update main.js

--- SynergismSaveEditor/main.js
+++ SynergismSaveEditor/main.js
@@ -2,32 +2,59 @@ const Field = document.getElementById("Field");
 const SupportedVersions = ["2.1.2", "2.7.0"];
 const Data = [
     {Name: "Coins", Index: "coins", Type: "number"},
+    {Name: "Coins this Prestige", Index: "coinsThisPrestige", Type: "number"},
+    {Name: "Coins this Transcension", Index: "coinsThisTranscension", Type: "number"},
+    {Name: "Coins this Reincarnation", Index: "coinsThisReincarnation", Type: "number"},
+    {Name: "Total coins", Index: "coinsTotal", Type: "number"},
     {Name: "Diamonds", Index: "prestigePoints", Type: "number"},
+    {Name: "Mythos", Index: "transcendPoints", Type: "number"},
+    {Name: "Particals", Index: "reincarnationPoints", Type: "number"},
+    {Name: "Crystals", Index: "prestigeShards", Type: "number"},
+    {Name: "Mythos shards", Index: "transcendShards", Type: "number"},
+    {Name: "Atoms", Index: "reincarnationShards", Type: "number"},
+    {Name: "Obtainium", Index: "researchPoints", Type: "number"},
+    {Name: "Offerings", Index: "runeshards", Type: "number"},
 ];
+
+for (let i = 0; i < Data.length; i++) {
+    document.getElementById(`${Data[i]["Index"]}EditTitle`).innerHTML = Data[i]["Name"];
+}
 
 let Items = [];
 let loaded = false;
 let jsonData = {};
 
+function ListSupportedVersions() {
+    let str = "Supported versions\n\n";
+    for (let i = SupportedVersions.length - 1; i > -1; i--) {
+        if (i == 0)
+            str += `v${SupportedVersions[i]}`;
+        else 
+            str += `v${SupportedVersions[i]}\n`;
+    }
+    console.log(str);
+}
+
 function Submit() {
     let save = Field.value;
     let pe = "";
     try {
-        pe = "Incorrect save file format";
+        pe = "Incorrect save file format. Error #100";
         let decoded = atob(save);
-        pe = "Could not decode due to data not being a dictionary.";
+        pe = "Could not decode due to data not being a dictionary. Error #101";
         let json = JSON.parse(decoded);
-        pe = "Could not find game version.";
+        pe = "Could not find game version. Error #102";
         let version = json["version"];
         if (version == undefined) throw "Not found.";
-        pe = "This version of Synergism is not supported."
+        pe = `Version v${version} of Synergism is not currently supported. Error #103`;
         if (!SupportedVersions.includes(version)) throw "Unsupported.";
         Items = [];
         for (let i = 0; i < Data.length; i++) {
             let d = Data[i]
-            pe = `Cannot find value at index "${d["Index"]}" of type "${d["Type"]}"`;
+            pe = `Cannot find value at index "${d["Index"]}" of type "${d["Type"]}". Error #104`;
             let val = json[d["Index"]];
             if (val == undefined) throw "Not found";
+            pe = "Unknown problem. Error #105";
             Items[i] = new Item(d, val);
         }
         loaded = true;
@@ -56,21 +83,85 @@ function SubmitChange(Value) {
                 in1 = document.getElementById(`${Value}Input1`);
                 val = ConvertNumber(Number(in0.value), in1.value, in0 == undefined || in1 == undefined);
                 jsonData[Value] = val;
-                for (let i = 0; i < Items.length; i++) {
-                    Items[i].Update();
-                }
+                break;
+            case "coinsThisPrestige":
+                in0 = document.getElementById(`${Value}Input0`);
+                in1 = document.getElementById(`${Value}Input1`);
+                val = ConvertNumber(Number(in0.value), in1.value, in0 == undefined || in1 == undefined);
+                jsonData[Value] = val;
+                break;
+            case "coinsThisTranscension":
+                in0 = document.getElementById(`${Value}Input0`);
+                in1 = document.getElementById(`${Value}Input1`);
+                val = ConvertNumber(Number(in0.value), in1.value, in0 == undefined || in1 == undefined);
+                jsonData[Value] = val;
+                break;
+            case "coinsThisReincarnation":
+                in0 = document.getElementById(`${Value}Input0`);
+                in1 = document.getElementById(`${Value}Input1`);
+                val = ConvertNumber(Number(in0.value), in1.value, in0 == undefined || in1 == undefined);
+                jsonData[Value] = val;
+                break;
+            case "coinsTotal":
+                in0 = document.getElementById(`${Value}Input0`);
+                in1 = document.getElementById(`${Value}Input1`);
+                val = ConvertNumber(Number(in0.value), in1.value, in0 == undefined || in1 == undefined);
+                jsonData[Value] = val;
                 break;
             case "prestigePoints":
                 in0 = document.getElementById(`${Value}Input0`);
                 in1 = document.getElementById(`${Value}Input1`);
                 val = ConvertNumber(Number(in0.value), in1.value, in0 == undefined || in1 == undefined);
                 jsonData[Value] = val;
-                for (let i = 0; i < Items.length; i++) {
-                    Items[i].Update();
-                }
+                break;
+            case "transcendPoints":
+                in0 = document.getElementById(`${Value}Input0`);
+                in1 = document.getElementById(`${Value}Input1`);
+                val = ConvertNumber(Number(in0.value), in1.value, in0 == undefined || in1 == undefined);
+                jsonData[Value] = val;
+                break;
+            case "reincarnationPoints":
+                in0 = document.getElementById(`${Value}Input0`);
+                in1 = document.getElementById(`${Value}Input1`);
+                val = ConvertNumber(Number(in0.value), in1.value, in0 == undefined || in1 == undefined);
+                jsonData[Value] = val;
+                break;
+            case "prestigeShards":
+                in0 = document.getElementById(`${Value}Input0`);
+                in1 = document.getElementById(`${Value}Input1`);
+                val = ConvertNumber(Number(in0.value), in1.value, in0 == undefined || in1 == undefined);
+                jsonData[Value] = val;
+                break;
+            case "transcendShards":
+                in0 = document.getElementById(`${Value}Input0`);
+                in1 = document.getElementById(`${Value}Input1`);
+                val = ConvertNumber(Number(in0.value), in1.value, in0 == undefined || in1 == undefined);
+                jsonData[Value] = val;
+                break;
+            case "reincarnationShards":
+                in0 = document.getElementById(`${Value}Input0`);
+                in1 = document.getElementById(`${Value}Input1`);
+                val = ConvertNumber(Number(in0.value), in1.value, in0 == undefined || in1 == undefined);
+                jsonData[Value] = val;
+                break;
+            case "researchPoints":
+                in0 = document.getElementById(`${Value}Input0`);
+                in1 = document.getElementById(`${Value}Input1`);
+                val = ConvertNumber(Number(in0.value), in1.value, in0 == undefined || in1 == undefined);
+                jsonData[Value] = val;
+                break;
+            case "runeshards":
+                in0 = document.getElementById(`${Value}Input0`);
+                in1 = document.getElementById(`${Value}Input1`);
+                val = ConvertNumber(Number(in0.value), in1.value, in0 == undefined || in1 == undefined);
+                jsonData[Value] = val;
                 break;
             default:
                 break;
+        }
+
+        for (let i = 0; i < Items.length; i++) {
+            Items[i].Update(true);
         }
     }
 }
